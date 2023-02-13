998a12dcabe7dca95ef352245c292dd6e2f7be3a
Deanbert <deanfay2000@gmail.com>
2023-02-13 07:11:50 +0000

    Collect sales data for the last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -56,26 +56,6 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
-    print("updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
-    print("updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
-
-
 def update_worksheet(data, worksheet):
     """
     Receives a list of integers to be inserted into a worksheet
@@ -103,7 +83,24 @@ def calculate_surplus_data(sales_row):
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
+
     return surplus_data
+
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from the sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists. 
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
 def main():
@@ -116,9 +113,8 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
 
-print("Welcome to Love Sandwiches Data Automation")
-main()
 
-"""
-Continue from Love Sandwiches Walkthrough Project > Calculating Surplus Data > Refactoring our Update Functions
-"""
+print("Welcome to Love Sandwiches Data Automation")
+#main()
+
+sales_column = get_last_5_entries_sales()
